abd4a0b7853fa89c51269fd42a933663c2f29adb
Paola Greco <paola.greco@telegraph.co.uk>
2020-05-16 09:49:05 +0100

    first commit

---
+++ src/test/java/org/hella/solutions/springreactive/SpringReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hella.solutions.springreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
